343db0b4875748bc426fd2e1c9619241b3a884a6
pravandkatyare <36467951+pravandkatyare@users.noreply.github.com>
2021-05-18 22:12:53 +0530

    changes updated

--- app/app.go
+++ app/app.go
@@ -10,8 +10,8 @@ var (
 	server = gin.Default()
 )
 
-// StartApplication initializes the application
-func StartApplication() {
+// StartApplication initializes the server and start application
+func startApplication() {
 	logging.Infof("Starting application on port: %s", viper.GetString("port"))
 	mapURLs()
 	logging.Fatalf("Error while starting application: %s", server.Run(":"+viper.GetString("port")))
